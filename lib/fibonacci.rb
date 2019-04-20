b0a35dba30f8dc42679e714c1b81a895936381dd
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-20 14:52:50 -0700

    COmpleted Fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,27 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: 0(n), where n is the given number
+# Space complexity: O(1)
 def fibonacci(n)
-  raise NotImplementedError
+  # raise NotImplementedError
+
+  if n == nil || n < 0
+    raise ArgumentError
+  else
+    # a = 0
+    # b = 1
+    # for i in (0...n / 2)
+    #   # print "#{a}, #{b}, "
+    #   a += b
+    #   b += a
+      
+    # end
+
+    f = ((1.618034 ** n) - ((-0.618034) ** n)) / Math.sqrt(5)
+  end
+
+  return f.round(0)
 end
+
+puts fibonacci(0)
